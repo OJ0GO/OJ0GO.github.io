89a828b320167ca0e1b798e9a0e0899eb51cb36c
Matheus Kauan Rodrigues de Souza <matheus.souza@upper.net.br>
2025-01-03 15:06:44 -0300

    cavaludo

--- js/script.js
+++ js/script.js
@@ -1,23 +1,96 @@
+var answerGroupElement = document.getElementById("mainItems");
 var chosenAnswerElement = document.getElementById("chosenAnswer");
 var checkingNull = false;
-function Sortear(){
+var answerQuantity = 2;
+var recarregamentoInicial = false;
+var cavaludo = document.getElementById("cavaludo");
+
+function ResetarRespostas(){
     if(checkingNull == true) return;
-    var firstAnswer = document.getElementById("firstAnswer").value;
-    var secondAnswer = document.getElementById("secondAnswer").value;
-    if(!firstAnswer || !secondAnswer) {
-        checkingNull = true;
-        chosenAnswerElement.innerHTML = `Preencha os campos necessários.`;
+    checkingNull = true;
+    LimparResposta();
+    answerQuantity = 2;
+    checkingNull = false;
+    RecarregarInputs();
+}
+
+function LimparResposta(){
+    chosenAnswerElement.innerHTML = ``;
+}
+
+function RecarregarInputs(){
+    if(checkingNull == true) return;
+    recarregamentoInicial = true;
+    LimparResposta();
+    answerGroupElement.innerHTML = ``;
+    for(let i = 0; i < answerQuantity; i++){
+        answerGroupElement.innerHTML += `
+            <input type="text" id="answer${i}" placeholder="Opção ${i+1}">
+        `;
+    }
+}
+
+RecarregarInputs();
+
+function Sortear() {
+    if (checkingNull) return;
+    checkingNull = true;
+    LimparResposta();
+    let answers = [];
+    for (let i = 0; i < answerQuantity; i++) {
+        let answer = document.getElementById(`answer${i}`).value;
+        if (!answer) {
+            showTemporaryMessage("Preencha todos os campos antes de sortear.", 1000);
+            return;
+        }
+        answers.push(answer);
+    }
+    cavaludo.classList.add("girando");
+    let randomNumber = Math.floor(Math.random() * answers.length);
+    console.log(randomNumber, answers[randomNumber]);
+    checkingNull = false;
+    showAnswer(answers[randomNumber]);
+}
+
+function showAnswer(mensagem) {
+    checkingNull = true;
+    mensagem = `O MARCELO ESCOLHEU... ${mensagem}`;
+    chosenAnswerElement.innerHTML = ''; 
+    let accumulatedDelay = 0;
+
+    for (let i = 0; i < mensagem.length; i++) {
+        let randomInterval = Math.floor(Math.random() * 50) + 100;
+        accumulatedDelay += randomInterval; 
         setTimeout(() => {
-            chosenAnswerElement.innerHTML = ``;
-            checkingNull = false;
-        }, 1000);
-        return;
+            chosenAnswerElement.innerHTML += mensagem[i];
+            
+            if(i == mensagem.length-1){
+                checkingNull = false;
+                showTemporaryMessage(mensagem, 3000);
+            }
+        }, accumulatedDelay);
     }
 
-    var randomNumber = Math.floor(Math.random() * 2) + 1;
-    if(randomNumber == 1){
-        chosenAnswerElement.innerHTML = `${firstAnswer}`;
-    }else{
-        chosenAnswerElement.innerHTML = `${secondAnswer}`;
+}
+
+function showTemporaryMessage(message, duration) {
+    checkingNull = true;
+    cavaludo.classList.remove("girando");
+    chosenAnswerElement.innerHTML = message;
+    setTimeout(() => {
+        chosenAnswerElement.innerHTML = ``;
+        checkingNull = false;
+    }, duration);
+}
+
+function AdicionarInputParaSorteio() {
+    if(answerQuantity >= 10) {
+        showTemporaryMessage(`Você chegou ao máximo de opções permitidas`, 1000);
+        return;
     }
+        
+    answerGroupElement.innerHTML += `
+        <input type="text" id="answer${answerQuantity}" placeholder="Opção ${answerQuantity + 1}">
+    `;
+    answerQuantity += 1; 
 }
